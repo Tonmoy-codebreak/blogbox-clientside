62416821b8dcfdb90b2ca99dfb3e30334d05056b
Tonmoy <tonmoyallalone@gmail.com>
2025-08-10 15:41:16 +0600

    updated section name

--- src/Components/HomePage/RecentBlogs.jsx
+++ src/Components/HomePage/RecentBlogs.jsx
@@ -81,7 +81,7 @@ const RecentBlogs = ({ isDark }) => {
         <h1
           className={`text-center text-4xl sm:text-5xl font-semibold text-blue-600 font-title mb-14 lg:pt-20`}
         >
-          Recent Blogs
+          New Blogs
         </h1>
 
         <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-10">
